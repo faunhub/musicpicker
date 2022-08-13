3ebbe2d373ef69f94b69f7fe5e75267951e49e8a
faunhub <76247482+faunhub@users.noreply.github.com>
2022-08-14 02:17:26 +0700

    added javascript selectors to elements

---
+++ script.js
@@ -0,0 +1,16 @@
+function nextQuestion() {
+
+}
+
+
+
+
+
+
+/*main */
+const questionCounter = document.querySelector('.counter');
+const question = document.querySelector('.question');
+const nextQuestionButton = document.querySelector('.nextQ');
+
+nextQuestionButton.addEventListener('click', nextQuestion);
+
