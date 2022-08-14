e5ab8bf129bcf2c23eda056a008ec0e727968214
faunhub <76247482+faunhub@users.noreply.github.com>
2022-08-14 20:45:53 +0700

    added changing answers

--- script.js
+++ script.js
@@ -23,22 +23,58 @@ let questionsList = [
         choice2: 'fs',
         choice3: '4',
     },
+    {
+        question: 'What is 2+6?',
+        choice1: 'c',
+        choice2: 'fs',
+        choice3: 'dsa',
+    },
+    {
+        question: 'What is 2+7?',
+        choice1: 'cdsd',
+        choice2: 'fs',
+        choice3: '4',
+    },
+    {
+        question: 'What is 2+8?',
+        choice1: 'c',
+        choice2: 'fs',
+        choice3: '434',
+    },
+    {
+        question: 'What is 2+9?',
+        choice1: 'cds',
+        choice2: 'fs',
+        choice3: '4cn',
+    },
+    {
+        question: 'What is 2+10?',
+        choice1: 'c1',
+        choice2: 'fs',
+        choice3: '468',
+    },
 ]
 
 function nextQuestion(){
     let currentNo = parseInt(questionCounter.textContent);
-    if (currentNo >=5){
+    if (currentNo >=10){
         window.location.href = 'result.html';
     }
     questionCounter.textContent = currentNo+1;
     currentQuestion.textContent = questionsList[currentNo-1].question;
+    answer1.textContent = questionsList[currentNo-1].choice1;
+    answer2.textContent = questionsList[currentNo-1].choice2;
+    answer3.textContent = questionsList[currentNo-1].choice3;
+
 }
 
 /*main */
 const questionCounter = document.querySelector('.counter');
 const currentQuestion = document.querySelector('.question');
 const nextQuestionButton = document.querySelector('.nextQ');
-
+const answer1 = document.querySelector('.option1');
+const answer2 = document.querySelector('.option2');
+const answer3 = document.querySelector('.option3');
 
 nextQuestionButton.addEventListener('click', nextQuestion);
 
